1cfe90bea222d068cc6b08a4cb3dc957805734f3
Danilo Bertolini <danilocbertolini@gmail.com>
2024-08-31 11:55:18 -0300

    server connection io

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
-import app from './app'
+import app from './app.js'
+import http from 'http'
+import { Server } from 'socket.io';
 
 const PORT = 3001;
+const server = http.createServer(app)
+const io = new Server(server, {cors: {origin: 'http://localhost:5173'}});
 
-app.listen(PORT, async () => {
+io.on('connection', socket => {
+  console.log(socket.id)
+})
+
+server.listen(PORT, async () => {
   console.log(`Rodando na porta ${PORT}`)
 })
