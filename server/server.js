aae49f166dc579fd5d539447f16c9830a71fe8f6
Danilo Bertolini <danilocbertolini@gmail.com>
2024-09-05 23:21:54 -0300

    receive message and set username socket

--- server/server.js
+++ server/server.js
@@ -8,6 +8,19 @@ const io = new Server(server, {cors: {origin: 'http://localhost:5173'}});
 
 io.on('connection', socket => {
   console.log(socket.id)
+
+  socket.on('set_username', username => {
+    socket.data.username = username
+    console.log('socket.data.username', socket.data.username)
+  })
+
+  socket.on('message', text => {
+    io.emit('receive_message', {
+      text,
+      authorId: socket.id,
+      author: socket.data.username
+    })
+  })
 })
 
 server.listen(PORT, async () => {
